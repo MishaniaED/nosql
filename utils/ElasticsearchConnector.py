b04fba4f99fa3c6742be21964ac5927025b20d74
Mikhail <misha.evkarpiev@mail.ru>
2023-12-28 18:25:28 +0300

    Work on connectors

--- utils/ElasticsearchConnector.py
+++ utils/ElasticsearchConnector.py
@@ -1,5 +1,7 @@
 import os
-from elasticsearch import AsyncElasticsearch
+from asyncio import exceptions
+
+from elasticsearch import AsyncElasticsearch, logger, NotFoundError
 
 elasticsearch_client: AsyncElasticsearch = None
 
@@ -24,3 +26,87 @@ async def close_elasticsearch_connect():
     if elasticsearch_client is None:
         return
     await elasticsearch_client.close()
+
+
+# def create_train_index():
+#     """Creating an index for trains"""
+#     try:
+#         body = {
+#             "mappings": {
+#                 "properties": {
+#                     "id": {"type": "integer"},
+#                     "route_id": {"type": "integer"},
+#                     "type": {"type": "keyword"},
+#                     "departure_date": {"type": "date"},
+#                     "arrival_date": {"type": "date"},
+#                 }
+#             }
+#         }
+#         response = elasticsearch_client.indices.create(index="trains", body=body, ignore=400)
+#         logger.info("Train index created successfully.")
+#         return response
+#     except Exception as e:
+#         logger.error(f"Error creating train index: {e}")
+#         return None
+#
+#
+# async def add_train_to_index(train_data):
+#     """Adding train information to the index"""
+#     try:
+#         route_id = train_data["route_id"]
+#         route = await db.routes.find_one({"route_id": route_id})
+#         stations = route["stations"]
+#         train_data["stations"] = stations
+#
+#         response = elasticsearch_client.index(index="trains", body=train_data)
+#         logger.info("Train data added to index successfully.")
+#         return response
+#     except Exception as e:
+#         logger.error(f"Error adding train data to index: {e}")
+#         return None
+
+
+def search_trains(departure_station_id, arrival_station_id, departure_date):
+    """Search for trains according to the specified criteria"""
+    try:
+        query = {
+            "bool": {
+                "must": [
+                    {
+                        "bool": {
+                            "should": [
+                                {"term": {"stations": departure_station_id}},
+                                {"term": {"stations": arrival_station_id}},
+                            ],
+                            "minimum_should_match": 2,
+                        }
+                    },
+                    {"range": {"departure_date": {"gte": departure_date}}},
+                ]
+            }
+        }
+        response = elasticsearch_client.search(index="trains", body={"query": query})
+        logger.info("Search for trains executed successfully.")
+        return [hit["_source"] for hit in response["hits"]["hits"]]
+    except Exception as e:
+        logger.error(f"Error searching trains: {e}")
+        return []
+
+
+def delete_train_from_index(train_id):
+    """Deleting train information from the index by ID"""
+    try:
+        query = {
+            "query": {
+                "term": {"id": train_id}
+            }
+        }
+        response = elasticsearch_client.delete_by_query(index="trains", body=query)
+        logger.info("Train data deleted from index successfully.")
+        return response
+    except NotFoundError:
+        logger.error(f"Train data not found in index for id: {train_id}")
+        return None
+    except Exception as e:
+        logger.error(f"Error deleting train data from index: {e}")
+        return None
--- utils/HazelcastConnector.py
+++ utils/HazelcastConnector.py
@@ -1,30 +1,76 @@
 import logging
+import coloredlogs
 import hazelcast
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("hazelcast")
+coloredlogs.install()
 
 
 def get_hazelcast_client():
-    client = hazelcast.HazelcastClient()
-    logger.info("Connected to cluster")
-    return client
+    try:
+        client = hazelcast.HazelcastClient()
+        logger.info("Connected to the Hazelcast cluster")
+        return client
+    except Exception as e:
+        logger.error(f"Error connecting to Hazelcast cluster: {e}")
+        raise
 
 
 hazelcast_client = get_hazelcast_client()
 
 
 def cache_station(station_id, station_data):
-    pass
+    try:
+        stations_map = hazelcast_client.get_map("stations")
+        future_station_data = stations_map.put(station_id, station_data)
+        result = future_station_data.result()
+        logger.info(f"Station data cached for station ID: {station_id}")
+        return result
+    except Exception as e:
+        logger.error(f"Error caching station data for station ID {station_id}: {e}")
+        raise
 
 
 async def get_cached_station(station_id):
-    pass
+    try:
+        stations_map = hazelcast_client.get_map("stations")
+        future_station_data = stations_map.get(station_id)
+        station_data = future_station_data.result()
+
+        if station_data is None:
+            logger.warning(f"No data found in cache for station ID: {station_id}")
+            return None
+
+        logger.info(f"Retrieved data from cache for station ID: {station_id}")
+        return station_data
+    except Exception as e:
+        logger.error(f"Error retrieving station data for station ID {station_id}: {e}")
+        raise
 
 
 def lock_ticket(ticket_id):
-    pass
+    try:
+        lock_key = f"myLock@group{ticket_id}"
+        ticket_lock = hazelcast_client.cp_subsystem.get_lock(lock_key).blocking()
+        fence = ticket_lock.try_lock()
+
+        if fence != ticket_lock.INVALID_FENCE:
+            logger.info(f"Ticket ID {ticket_id} locked successfully.")
+            return True
+        logger.warning(f"Ticket ID {ticket_id} is already locked.")
+        return False
+    except Exception as e:
+        logger.error(f"Error locking ticket ID {ticket_id}: {e}")
+        raise
 
 
 def unlock_ticket(ticket_id):
-    pass
+    try:
+        lock_key = f"myLock@group{ticket_id}"
+        ticket_lock = hazelcast_client.cp_subsystem.get_lock(lock_key).blocking()
+        ticket_lock.unlock()
+        logger.info(f"Ticket ID {ticket_id} unlocked successfully.")
+    except Exception as e:
+        logger.error(f"Error unlocking ticket ID {ticket_id}: {e}")
+        raise
