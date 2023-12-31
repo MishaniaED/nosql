5c14f7f42da9137d425151b50d97391b17df3213
Mikhail <misha.evkarpiev@mail.ru>
2023-12-31 03:15:53 +0300

    fixed ES startup errors

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import uvicorn
 from handler.EventHandlers import startup, shutdown
 
 app = FastAPI()
-app.add_event_handler("startup", startup)
-app.add_event_handler("shutdown", shutdown)
+
+# app.add_event_handler("startup", startup)
+# app.add_event_handler("shutdown", shutdown)
 
 
 if __name__ == "__main__":
